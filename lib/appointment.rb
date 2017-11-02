943c42e3a315bdbcf72a4c759e5ecdcfea79c152
Amanda Ackerman <ackerm44@gmail.com>
2017-11-02 19:02:28 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+  end
+
+  def patient
+    patient.add_appointment(self)
+  end
+
+  def doctor
+    doctor.add_appointment(self)
+  end
+end
